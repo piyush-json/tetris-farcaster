55b6f8b7d833e65e0be586d269f5923034e21361
piyush-panpaliya <panpaliyapiyush0@gmail.com>
2025-08-15 19:26:29 +0530

    sdk fix

--- src/components/Tetris.tsx
+++ src/components/Tetris.tsx
@@ -1,10 +1,18 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import StartPage from "./StartPage";
 import Game from "./Game";
+import { useMiniKit } from "@coinbase/onchainkit/minikit";
 
 const Tetris: React.FC = () => {
+  const { setFrameReady, isFrameReady } = useMiniKit();
+
   const [running, setRunning] = useState(false);
+  useEffect(() => {
+    if (!isFrameReady) {
+      setFrameReady();
+    }
+  }, [setFrameReady, isFrameReady]);
   return running ? (
     <Game stopClick={() => setRunning(false)} />
   ) : (
